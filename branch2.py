342c59c1501e2f7ed520c925a04f48d0d4249215
hemanthkumar882002 <hemanthkumar882002@gmail.com>
2026-06-03 16:55:04 +0530

    csv file added

--- branch2.py
+++ branch2.py
@@ -1,2 +1,12 @@
+
 print ('hemanth kumar r')
 print ("aishu")
+import  pandas
+import pandas as pd
+df=pd.read_csv(r"C:\Users\AISHU\Music\orders.csv")
+df
+df=pd.read_csv("C:/Users/AISHU/Music/orders.csv")
+
+df
+
+
